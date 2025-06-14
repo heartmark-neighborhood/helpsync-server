1b963f4837c54fd9583ab9a4946c11c19d1deda5
4geniu <taketubo0714@gmail.com>
2025-06-14 18:38:05 +0000

    feat: Implement createHelpRequest function for handling help request creation

---
+++ functions/src/infrastructure/functions/create-help-request.function.ts
@@ -0,0 +1,58 @@
+import { https } from "firebase-functions";
+import * as logger from "firebase-functions/logger";
+import { getFirestore } from "firebase-admin/firestore";
+
+import { z } from "zod";
+
+import { CreateHelpRequestInputSchema } from "../../domain/help-request/create-help-request.shcema";
+import { CreateHelpRequestUseCase } from "../../domain/help-request/create-help-request.usecase";
+import { HelpRequestRepository } from "../firestore/help-request.repository";
+import { UserRepository } from "../firestore/User.repository";
+import { FcmGateway } from "../notifications/fcm-gateway";
+import { ProximityVerificationNotifier } from "../notifications/proximity-verification-notifier";
+import { UserId } from "../../domain/user/user-id.value";
+
+
+export const createHelpRequest = https.onCall(
+  async (request) => {
+    if(!request.auth) {
+      logger.error("Unauthorized request");
+      throw new https.HttpsError("unauthenticated", "Unauthorized request");
+    }
+
+    try {
+      const input = CreateHelpRequestInputSchema.parse(request.data);
+      const db = getFirestore();
+      const helpRequestRepository = HelpRequestRepository.create(db);
+      const userRepository = UserRepository.create(db);
+      const notifier = ProximityVerificationNotifier.create(FcmGateway.create());
+
+      const usecase = CreateHelpRequestUseCase.create(
+        helpRequestRepository,
+        userRepository,
+        notifier
+      );
+
+      const requesterId = UserId.create(request.auth.uid);
+
+      const helpRequest = await usecase.execute({
+        ...input
+      }, requesterId);
+      logger.info("Help request created successfully", { uid: request.auth.uid, helpRequestId: helpRequest.id });
+
+      return { id: helpRequest.id };
+
+    } catch (error) {
+
+      if (error instanceof z.ZodError) {
+        logger.warn('Validation error:', { uid: request.auth?.uid, errors: error.errors });
+        throw new https.HttpsError('invalid-argument', 'The data provided is invalid.');
+      }
+
+      logger.error('Internal error:', { uid: request.auth?.uid, error });
+      throw new https.HttpsError('internal', 'An internal server error occurred.');
+      
+    }
+  }
+)
+
