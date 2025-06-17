e04f4dcae6100b9678b6d188da311f6b08b465d6
4geniu <taketubo0714@gmail.com>
2025-06-17 05:53:15 +0000

    feat: Add findAvailableSupporters method to IUserRepository and UserRepository

--- functions/src/domain/user/i-user.repository.ts
+++ functions/src/domain/user/i-user.repository.ts
@@ -1,10 +1,12 @@
 import { User } from "./User.entity";
 import { UserId } from "./user-id.value";
+import { Location } from "../shared/value-object/Location.value";
 
 
 export interface IUserRepository {
   findById(id: UserId): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
+  findAvailableSupporters(location: Location, radiusInM: number): Promise<User[]>;
   save(user: User): Promise<void>;
   delete(user: User): Promise<void>;
 }
--- functions/src/infrastructure/firestore/User.repository.ts
+++ functions/src/infrastructure/firestore/User.repository.ts
@@ -1,6 +1,7 @@
 import { IUserRepository } from "../../domain/user/i-user.repository"; 
 import { User } from "../../domain/user/User.entity";
 import { UserId } from "../../domain/user/user-id.value";
+import { Location } from "../../domain/shared/value-object/Location.value";
 
 export class UserRepository implements IUserRepository {
   private db: FirebaseFirestore.Firestore;
@@ -22,6 +23,10 @@ export class UserRepository implements IUserRepository {
   }
 
   async findByEmail(email: string): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
+
+  async findAvailableSupporters(location: Location, radiusInM: number): Promise<User[]> {
     throw new Error("Method not implemented.");
   }
 
