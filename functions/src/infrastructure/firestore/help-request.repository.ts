3fd328871af8931d86ada51bfadf14780731b92c
4geniu <taketubo0714@gmail.com>
2025-06-22 17:33:50 +0000

    feat: Implement save method in HelpRequestRepository to persist help requests and associated candidates

--- functions/src/infrastructure/firestore/help-request.repository.ts
+++ functions/src/infrastructure/firestore/help-request.repository.ts
@@ -4,6 +4,7 @@ import { HelpRequest } from '../../domain/help-request/help-request.entity';
 import { IHelpRequestRepository } from '../../domain/help-request/i-help-request.repository';
 import { HelpRequestId } from '../../domain/help-request/help-request-id.value';
 import { CreateHelpRequestCommand } from '../../domain/help-request/create-help-request.usecase';
+import { User } from '../../domain/user/User.entity';
 
 export class HelpRequestRepository implements IHelpRequestRepository {
   private db: FirebaseFirestore.Firestore;
@@ -16,8 +17,36 @@ export class HelpRequestRepository implements IHelpRequestRepository {
     this.db = db;
   }
 
-  async save(helpRequest: HelpRequest): Promise<HelpRequest> {
-    throw new Error('Method not implemented.');
+  async save(helpRequest: HelpRequest, requester: User): Promise<HelpRequest> {
+    
+    const batch = this.db.batch();
+    const helpRequestRef = this.db.collection('helpRequests').doc(helpRequest.id.value);
+
+    const { status, location, createdAt, updatedAt } = helpRequest.toPersistenceModel();
+    batch.set(helpRequestRef, {
+      id: helpRequest.id.value,
+      requesterId: requester.id.value,
+      status,
+      location: {
+        latitude: location.latitude,
+        longitude: location.longitude,
+        geohash: location.calcGeohash()
+      },
+      createdAt,
+      updatedAt
+    });
+
+    const candidatesRef = helpRequestRef.collection('candidates');
+    helpRequest.candidatesCollection.all.forEach((candidate) => {
+      const { candidateId, status } = candidate.toPersistenceModel();
+      batch.set(candidatesRef.doc(candidateId), {
+        candidateId,
+        status
+      });
+    });
+
+    await batch.commit();
+    return helpRequest;
   }
 
   async findById(id: HelpRequestId): Promise<HelpRequest | null> {
