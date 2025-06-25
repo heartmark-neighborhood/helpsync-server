83cb86b2cc97fd40403ead5eb5091df781387a93
4geniu <taketubo0714@gmail.com>
2025-06-25 14:11:06 +0000

    refactor: Export UserIdSchema and update create method to use UserIdPersistenceModel

--- functions/src/domain/user/user-id.value.ts
+++ functions/src/domain/user/user-id.value.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const UserIdSchema = z
+export const UserIdSchema = z
   .string({
     required_error: 'User ID is required.',
     invalid_type_error: 'User ID must be a string.',
@@ -8,11 +8,13 @@ const UserIdSchema = z
   .min(1, { message: 'User ID cannot be empty.' }) 
   .max(128, { message: 'User ID is too long.' }); 
 
+export type UserIdPersistenceModel = z.infer<typeof UserIdSchema>;
+
 
 export class UserId {
   private constructor(readonly value: string) {}
 
-  static create(value: string): UserId {
+  static create(value: UserIdPersistenceModel): UserId {
     UserIdSchema.parse(value);
     return new UserId(value);
   }
