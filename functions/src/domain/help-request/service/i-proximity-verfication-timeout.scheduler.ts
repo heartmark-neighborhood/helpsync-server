5aa4ab2b0adeee603c7da4a62b716e2dc50d4c77
4geniu <taketubo0714@gmail.com>
2025-06-26 13:09:01 +0000

    refactor: remove cancel method from IProximityVerificationTimeoutScheduler interface

--- functions/src/domain/help-request/service/i-proximity-verfication-timeout.scheduler.ts
+++ functions/src/domain/help-request/service/i-proximity-verfication-timeout.scheduler.ts
@@ -2,5 +2,4 @@ import { HelpRequestId } from "../help-request-id.value";
 
 export interface IProximityVerificationTimeoutScheduler {
   schedule(helpRequestId: HelpRequestId, timeoutAt: Date): Promise<void>;
-  cancel(helpRequestId: HelpRequestId): Promise<void>;
 }
