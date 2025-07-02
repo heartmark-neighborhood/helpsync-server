6289a98a50348fdaeff5f4fbc49e005f77364919
4geniu <taketubo0714@gmail.com>
2025-07-02 21:27:39 +0000

    feat: add UserInfo DTO and schema for requester information management

--- functions/src/domain/help-request/requester-info.dto.ts
+++
@@ -1,9 +0,0 @@
-import { DeviceId } from "../device/device-id.value";
-import { UserId } from "../user/user-id.value";
-
-export interface RequesterInfo {
-  id: UserId;
-  nickname: string;
-  iconUrl: string;
-  deviceId: DeviceId;
-}
---
+++ functions/src/domain/help-request/user-info.dto.ts
@@ -0,0 +1,39 @@
+import { DeviceId, DeviceIdSchema } from "../device/device-id.value";
+import { UserId, UserIdSchema } from "../user/user-id.value";
+
+import { z } from "zod";
+
+export const UserInfoSchema = z.object({
+  id: UserIdSchema,
+  nickname: z.string(),
+  physicalDescription: z.string(),
+  iconUrl: z.string(),
+  deviceId: DeviceIdSchema
+});
+
+export type UserInfoDTO = z.infer<typeof UserInfoSchema>;
+
+export class UserInfo {
+  private constructor(
+    public readonly id: UserId,
+    public readonly nickname: string,
+    public readonly physicalDescription: string,
+    public readonly iconUrl: string,
+    public readonly deviceId: DeviceId
+  ) {}
+
+  static create(dto: UserInfoDTO): UserInfo {
+    return new UserInfo(
+      UserId.create(dto.id),
+      dto.nickname,
+      dto.physicalDescription,
+      dto.iconUrl,
+      DeviceId.create(dto.deviceId)
+    );
+  }
+
+  static fromPersistenceModel(model: UserInfoDTO): UserInfo {
+    return UserInfo.create(model);
+  }
+
+}
