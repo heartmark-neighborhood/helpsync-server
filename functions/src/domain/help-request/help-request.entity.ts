6120acb0103436b1b69cd9cfccc565b91147a7bb
4geniu <taketubo0714@gmail.com>
2025-07-07 05:51:45 +0000

    feat: Implement complete method in HelpRequest class to finalize requests

--- functions/src/domain/help-request/help-request.entity.ts
+++ functions/src/domain/help-request/help-request.entity.ts
@@ -29,4 +29,17 @@ export class HelpRequest{
   ): HelpRequest {
     return new HelpRequest(helpRequestId, proximityVerificationId, requesterId, status, [], location, createdAt, updatedAt);
   }
+
+  complete(): HelpRequest {
+    return new HelpRequest(
+      this.id,
+      this.proximityVerificationId,
+      this.requesterId,
+      'COMPLETED',
+      this.matchedSupportersIds,
+      this.location,
+      this.createdAt,
+      new Date()
+    );
+  }
 }
