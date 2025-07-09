bd3a7d3892601f1d29e2fd510ea1b7d7007995a2
4geniu <taketubo0714@gmail.com>
2025-07-09 02:18:47 +0000

    feat: implement UpdateDeviceLocation use case for updating device locations

---
+++ functions/src/domain/device/update-device-location.usecase.ts
@@ -0,0 +1,43 @@
+import { Location, LocationSchema } from "../shared/value-object/Location.value";
+import { DeviceId, DeviceIdSchema } from "./device-id.value";
+import { IDeviceRepository } from "./i-device.repository";
+import { z } from "zod";
+
+export const UpdateDeviceLocationInputSchema = z.object({
+  deviceId: DeviceIdSchema,
+  location: LocationSchema,
+});
+
+export type UpdateDeviceLocationInput = z.infer<typeof UpdateDeviceLocationInputSchema>;
+
+export class UpdateDeviceLocationCommand {
+  private constructor(
+    public readonly deviceId: DeviceId,
+    public readonly location: Location,
+  ) {}
+
+  static create(input: UpdateDeviceLocationInput): UpdateDeviceLocationCommand {
+    const deviceId = DeviceId.create(input.deviceId);
+    const location = Location.create(input.location);
+
+    return new UpdateDeviceLocationCommand(deviceId, location);
+  }
+}
+
+
+export class UpdateDeviceLocationUseCase {
+  private constructor(private readonly repository: IDeviceRepository) {}
+  static create(repository: IDeviceRepository): UpdateDeviceLocationUseCase {
+    return new UpdateDeviceLocationUseCase(repository);
+  }
+
+  async execute(command: UpdateDeviceLocationCommand): Promise<void> {
+    const device = await this.repository.findById(command.deviceId);
+    if (!device) {
+      throw new Error(`Device with ID ${command.deviceId.value} not found`);
+    }
+
+    const updatedDevice = device.movedTo(command.location);
+    await this.repository.save(updatedDevice);
+  }
+}
