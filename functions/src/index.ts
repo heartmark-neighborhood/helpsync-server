433f1d44f88f7ae9e4de6e985b64c6643845145f
4geniu <taketubo0714@gmail.com>
2025-07-09 07:14:44 +0000

    refactor: remove commented-out helloWorld function and clean up imports

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,10 +10,8 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export { createHelpRequest } from "./infrastructure/functions/create-help-request.function";
+export { completeHelpRequest } from "./infrastructure/functions/complete-help-request.functions";
+export { handleProximityVerificationResult } from "./infrastructure/functions/handle-proximity-verification-result.function";
+export { onProximityVerificationTimeout } from "./infrastructure/functions/on-proximity-verification-timeout.function";
+export { updateDeviceLocation } from "./infrastructure/functions/update-device-location.function";
