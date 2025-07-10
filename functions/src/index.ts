a6a55b2e5c2f6e1a67fb5b2cfcb601c9e3807ee1
4geniu <taketubo0714@gmail.com>
2025-07-10 08:19:01 +0000

    fix: initialize Firebase admin SDK and update exports in index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,11 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import * as admin from "firebase-admin";
 
-export {createHelpRequest} from "./infrastructure/functions/create-help-request.function";
-export {completeHelpRequest} from "./infrastructure/functions/complete-help-request.functions";
-export {handleProximityVerificationResult} from "./infrastructure/functions/handle-proximity-verification-result.function";
-export {onProximityVerificationTimeout} from "./infrastructure/functions/on-proximity-verification-timeout.function";
-export {updateDeviceLocation} from "./infrastructure/functions/update-device-location.function";
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+
+export * from "./infrastructure/functions/create-help-request.function.js";
+export * from "./infrastructure/functions/complete-help-request.function.js";
+export * from "./infrastructure/functions/handle-proximity-verification-result.function.js";
+export * from "./infrastructure/functions/on-proximity-verification-timeout.function.js";
+export * from "./infrastructure/functions/update-device-location.function.js";
