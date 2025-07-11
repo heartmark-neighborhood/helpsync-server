3db0cb565505c85fe411801616b7e0407a6dd8e3
Kobaso <soya.k0622@icloud.com>
2025-07-11 12:11:20 +0900

    FCMトークン更新要求処理の実装

---
+++ functions/src/infrastructure/functions/renew-device-token.function.ts
@@ -0,0 +1,29 @@
+import { https, logger } from 'firebase-functions';
+import { getFirestore } from 'firebase-admin/firestore';
+
+import { RenewDeviceTokenCommand, RenewDeviceTokenSchema, RenewDeviceTokenUseCase } from '../../domain/device/renew-device-token.usecase';
+import { DeviceRepository } from '../firestore/device.repository';
+import { SystemClock } from '../service/SystemClock';
+
+const renewDeviceToken = https.onCall(async (request) => {
+  if(!request.auth){
+    throw new https.HttpsError('unauthenticated', 'Unauthenticated request');
+  }
+
+  const firestore = getFirestore();
+  const clock = SystemClock.create();
+  const repository = DeviceRepository.create(firestore, clock);
+
+  const usecase = RenewDeviceTokenUseCase.create(repository);
+
+  const parsedData = RenewDeviceTokenSchema.safeParse(request.data);
+  if (!parsedData.success) {
+    logger.error('Invalid input data', parsedData.error);
+    throw new https.HttpsError('invalid-argument', 'Invalid input data');
+  }
+  const command = RenewDeviceTokenCommand.create(parsedData.data);
+
+  logger.info('Executing UpdateDeviceLocationCommand', { command });
+  await usecase.execute(command);
+  return { success: true };
+});
